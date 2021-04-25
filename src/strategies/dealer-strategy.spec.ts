93021db8317ab04edd769fdc5f5a1ee45caef6db
James Tharpe <jimmy.tharpe@gmail.com>
2021-04-25 18:38:19 -0400

    Prettier

--- src/strategies/dealer-strategy.spec.ts
+++ src/strategies/dealer-strategy.spec.ts
@@ -1,29 +1,18 @@
 import expect from "expect";
-import {
-	aceOfSpades,
-	sevenOfSpades,
-	sixOfSpades, tenOfSpades,
-	twoOfSpades
-} from "../cards";
+import { aceOfSpades, sevenOfSpades, sixOfSpades, tenOfSpades, twoOfSpades } from "../cards";
 import { dealerStrategy } from "./dealer-strategy";
 import { Action } from "./strategy";
 
 describe("Dealer Strategy", () => {
-  it("Hits on Soft 17", () => {
-    expect(dealerStrategy(aceOfSpades, aceOfSpades, sixOfSpades)).toEqual(
-      Action.Hit
-    );
-  });
+	it("Hits on Soft 17", () => {
+		expect(dealerStrategy(aceOfSpades, aceOfSpades, sixOfSpades)).toEqual(Action.Hit);
+	});
 
-  it("Hits on hands < 17", () => {
-    expect(dealerStrategy(aceOfSpades, twoOfSpades, tenOfSpades)).toEqual(
-      Action.Hit
-    );
-  });
+	it("Hits on hands < 17", () => {
+		expect(dealerStrategy(aceOfSpades, twoOfSpades, tenOfSpades)).toEqual(Action.Hit);
+	});
 
-  it("Stays on hands >= 17", () => {
-    expect(dealerStrategy(aceOfSpades, sevenOfSpades, tenOfSpades)).toEqual(
-      Action.Stay
-    );
-  });
+	it("Stays on hands >= 17", () => {
+		expect(dealerStrategy(aceOfSpades, sevenOfSpades, tenOfSpades)).toEqual(Action.Stay);
+	});
 });
